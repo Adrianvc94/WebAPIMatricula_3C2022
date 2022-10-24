cfee6593bbc772efb71d9eda840a3b1398b40a91
andres <andresromero472@gmail.com>
2022-10-24 05:03:31 +0000

    feat: api horarior

---
+++ API.Bll.Horario/Interfaces/IAdHorario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Bll.Horario.Interfaces
+{
+    public interface IAdHorario
+    {
+        API.Dto.Horario.Salida.VerTodosHorarios VerTodosHorarios();
+        API.Dto.Horario.Salida.VerDetalleHorario VerDetalleHorario(API.Dto.Horario.Entrada.VerDetalleHorario pInformacion);
+        API.Dto.Horario.Salida.AgregarHorario AgregarHorario(API.Dto.Horario.Entrada.AgregarHorario pInformacion);
+        API.Dto.Horario.Salida.EditarHorario EditarHorario(API.Dto.Horario.Entrada.EditarHorario pInformacion);
+        API.Dto.Horario.Salida.EliminarHorario EliminarHorario(API.Dto.Horario.Entrada.EliminarHorario pInformacion);
+    }
+}
