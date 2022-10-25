33682bd3867428af5a6c814e117f6d190ca6613d
andres <andresromero472@gmail.com>
2022-10-25 04:29:27 +0000

    feat: api profesor

---
+++ API.Dto.Profesor/Salida/VerDetalleProfesor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Dto.Profesor.Salida
+{
+    public class VerDetalleProfesor : General.RespuestaAPI
+    {
+        public int Codigo { get; set; }
+        public string Identificacion { get; set; }
+        public string NombreCompleto { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Estado { get; set; }
+        public string CodigoColegiatura { get; set; }
+    }
+}
---
+++ API.Dto.Profesor/Salida/VerTodosProfesores.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace API.Dto.Profesor.Salida
+{
+    public class VerTodosProfesores : Dto.General.RespuestaAPI
+    {
+        public List<DatosProfesor> ListaProfesores { get; set; }
+
+
+
+        public VerTodosProfesores()
+        {
+            ListaProfesores = new List<DatosProfesor>();
+        }
+    }
+
+
+
+    public class DatosProfesor
+    {
+        public int Codigo { get; set; }
+        public string Identificacion { get; set; }
+        public string NombreCompleto { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Estado { get; set; }
+        public string CodigoColegiatura { get; set; }
+    }
+}
