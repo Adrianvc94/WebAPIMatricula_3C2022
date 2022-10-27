d74c60749be303f5c118a95ca8f8d8b0d47ee801
Andres Romero Zuniga <andresromero472@gmail.com>
2022-10-26 23:13:16 -0600

    feat: update store into single files

---
+++ store_agregar.sql
@@ -0,0 +1,192 @@
+USE [MatriculaULACIT_3C2022]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Agregar_Colegiatura]
+    @Nombre VARCHAR(50),
+    @Facultado VARCHAR(50),
+    @GradoAcademico VARCHAR(25),
+    @Acreditada VARCHAR(4)
+	
+AS BEGIN 
+	SET NOCOUNT ON;		
+
+	INSERT INTO Colegiatura(Nombre, Facultado, GradoAcademico, Acreditada)
+	SELECT 
+        @Nombre,
+        @Facultado,
+        @GradoAcademico,
+        @Acreditada	
+				
+	SELECT SCOPE_IDENTITY() AS Codigo
+		
+END 
+GO
+
+CREATE PROCEDURE [dbo].[Agregar_Curso]
+    @Nombre varchar(50),
+	@Creditos varchar(25),
+	@CodigoHorario int,
+	@Estado varchar(10)
+AS BEGIN 
+	SET NOCOUNT ON;		
+
+	INSERT INTO Curso(Nombre, Creditos, CodigoHorario, Estado)
+	SELECT
+        @Nombre,
+        @Creditos,
+        @CodigoHorario,
+        @Estado	
+	SELECT SCOPE_IDENTITY() AS Codigo
+		
+END 
+GO
+
+CREATE PROCEDURE [dbo].[Agregar_Error]
+	@CodigoUsuario INT,
+	@FechaHora Datetime, 
+	@Modulo VARCHAR(50), 
+	@Clase VARCHAR(50), 
+	@Metodo VARCHAR(50),
+	@Fuente VARCHAR(100),
+	@Numero INT,
+	@Excepcion VARCHAR(max)
+	
+AS BEGIN 
+	SET NOCOUNT ON;		
+
+	INSERT INTO Error(
+				CodigoUsuario,
+				FechaHora,
+				Modulo,
+				Clase,
+				Metodo,
+				Fuente,
+				Numero,
+				Excepcion)
+	SELECT 
+				@CodigoUsuario,
+				@FechaHora,
+				@Modulo,
+				@Clase,
+				@Metodo,
+				@Fuente,
+				@Numero,
+				@Excepcion	
+				
+	SELECT SCOPE_IDENTITY() AS Codigo
+		
+END 
+
+GO
+
+CREATE PROCEDURE [dbo].[Agregar_Estudiante]
+	@Identificacion VARCHAR(30), 
+	@NombreCompleto VARCHAR(100),
+	@CorreoElectronico VARCHAR(50),
+	@Estado VARCHAR(10)
+	
+AS BEGIN 
+	SET NOCOUNT ON;		
+
+	INSERT INTO Estudiante(Identificacion, NombreCompleto, CorreoElectronico, Estado)
+	SELECT 
+		@Identificacion, 
+		@NombreCompleto, 
+		@CorreoElectronico,
+		@Estado		
+				
+	SELECT SCOPE_IDENTITY() AS Codigo
+		
+END 
+GO
+
+CREATE PROCEDURE [dbo].[Agregar_Horario]
+    @Dia VARCHAR(15),
+	@HoraInicio VARCHAR(7),
+	@HoraFin VARCHAR(7),
+	@Sede VARCHAR(25),
+	@Aula VARCHAR(15)
+	
+AS BEGIN 
+	SET NOCOUNT ON;		
+
+	INSERT INTO Horario(Dia, HoraInicio, HoraFin, Sede, Aula)
+	SELECT 
+        @Dia,
+        @HoraInicio,
+        @HoraFin,
+        @Sede,	
+		@Aula		
+	SELECT SCOPE_IDENTITY() AS Codigo
+		
+END 
+GO
+
+CREATE PROCEDURE [dbo].[Agregar_Matricula]
+    @CodigoUsuario int,
+	@CodigoCurso int,
+	@CodigoEstudiante int,
+	@FechaHora datetime
+	
+AS BEGIN 
+	SET NOCOUNT ON;		
+
+	INSERT INTO Matricula(CodigoUsuario, CodigoCurso, CodigoEstudiante, FechaHora)
+	SELECT 
+        @CodigoUsuario,
+        @CodigoCurso,
+        @CodigoEstudiante,
+        @FechaHora	
+	SELECT SCOPE_IDENTITY() AS Codigo
+		
+END 
+GO
+
+CREATE PROCEDURE [dbo].[Agregar_Nota]
+    @CodigoCurso int,
+    @CodigoEstudiante int,
+    @CodigoProfesor int,
+    @Nota numeric,
+    @Estado VARCHAR(10)
+	
+AS BEGIN 
+	SET NOCOUNT ON;		
+
+	INSERT INTO Nota(CodigoCurso, CodigoEstudiante, CodigoProfesor, Nota, Estado)
+	SELECT 
+	    @CodigoCurso,
+        @CodigoEstudiante,
+        @CodigoProfesor,
+        @Nota,
+        @Estado	
+				
+	SELECT SCOPE_IDENTITY() AS Codigo
+		
+END 
+GO
+
+CREATE PROCEDURE [dbo].[Agregar_Profesor]
+    @Identificacion varchar(30),
+	@Nombre varchar(100),
+	@Correo varchar(50),
+	@Estado varchar(10),
+	@CodigoColegiatura int
+AS BEGIN 
+	SET NOCOUNT ON;		
+
+	INSERT INTO Profesor(Identificacion, Nombre, Correo, Estado, CodigoColegiatura)
+	SELECT @Identificacion,
+        @Nombre,
+        @Correo,
+        @Estado,
+        @CodigoColegiatura	
+	SELECT SCOPE_IDENTITY() AS Codigo
+		
+END 
+GO
---
+++ store_ver_todos.sql
@@ -0,0 +1,137 @@
+USE [MatriculaULACIT_3C2022]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[Ver_Todos_Colegiaturas]	
+
+AS BEGIN 
+	SET NOCOUNT ON;
+	SELECT 
+			Codigo,
+            Nombre,
+            Facultado,
+            GradoAcademico,
+            Acreditada
+	FROM Colegiatura
+	
+END 
+GO
+
+CREATE PROCEDURE [dbo].[Ver_Todos_Cursos]	
+
+AS BEGIN 
+	SET NOCOUNT ON;
+	SELECT 
+			Codigo,
+            Nombre,
+            Creditos,
+            CodigoHorario,
+            Estado
+	FROM Curso
+	
+END 
+GO
+
+CREATE PROCEDURE [dbo].[Ver_Todos_Errores]	
+
+AS BEGIN 
+	SET NOCOUNT ON;
+	SELECT 
+				Codigo,
+				CodigoUsuario,
+				FechaHora,
+				Modulo,
+				Clase,
+				Metodo,
+				Fuente,
+				Numero,
+				Excepcion
+	FROM Error
+	
+END 
+GO
+
+CREATE PROCEDURE [dbo].[Ver_Todos_Estudiantes]	
+
+AS BEGIN 
+	SET NOCOUNT ON;
+	SELECT 
+			Codigo,
+			Identificacion,
+			NombreCompleto,
+			CorreoElectronico,
+			Estado
+	FROM Estudiante
+	
+END 
+GO
+
+CREATE PROCEDURE [dbo].[Ver_Todos_Horarios]	
+
+AS BEGIN 
+	SET NOCOUNT ON;
+	SELECT 
+			Codigo,
+            Dia,
+            HoraInicio,
+            HoraFin,
+            Sede,
+            Aula
+	FROM Horario
+	
+END 
+GO
+
+CREATE PROCEDURE [dbo].[Ver_Todos_Matriculas]	
+
+AS BEGIN 
+	SET NOCOUNT ON;
+	SELECT 
+			Codigo,
+            CodigoUsuario,
+            CodigoCurso,
+            CodigoEstudiante,
+            FechaHora
+
+	FROM Matricula
+	
+END 
+GO
+
+CREATE PROCEDURE [dbo].[Ver_Todos_Profesores]	
+
+AS BEGIN 
+	SET NOCOUNT ON;
+	SELECT 
+			Codigo,
+            Identificacion,
+            Nombre,
+            Correo,
+            Estado,
+            CodigoColegiatura
+	FROM Profesor
+	
+END 
+GO
+
+CREATE PROCEDURE [dbo].[Ver_Todos_Notas]	
+
+AS BEGIN 
+	SET NOCOUNT ON;
+	SELECT 
+			Codigo,
+            CodigoCurso,
+            CodigoEstudiante,
+            CodigoProfesor,
+            Nota,
+            Estado
+	FROM Nota
+	
+END 
+GO
